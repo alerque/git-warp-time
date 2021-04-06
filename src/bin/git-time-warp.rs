6e937e36afc6fd49ccdb03506af8bd6eb1f16375
Caleb Maclennan <caleb@alerque.com>
2021-04-06 12:31:30 +0300

    Map out basic library ergonomics

--- src/bin/git-time-warp.rs
+++ src/bin/git-time-warp.rs
@@ -1,10 +1,15 @@
 use clap::IntoApp;
 use git_time_warp::cli::Cli;
+use git_time_warp::{get_repo, reset_mtime};
 use git_time_warp::{Result, VERSION};
 
 fn main() -> Result<()> {
     let app = Cli::into_app().version(VERSION);
-    let matches = app.get_matches();
-    println!("Hello, world! {:?}", matches);
+    app.get_matches();
+    let repo = get_repo()?;
+    let files = reset_mtime(repo)?;
+    for file in files.iter() {
+        println!("Rewound the clock: {}", file);
+    }
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use git2::Repository;
+use std::collections::HashSet;
 use std::{error, result};
 
 pub mod cli;
@@ -6,3 +8,33 @@ pub mod cli;
 pub static VERSION: &str = env!("VERGEN_GIT_SEMVER");
 
 pub type Result<T> = result::Result<T, Box<dyn error::Error>>;
+
+type FileSet = HashSet<String>;
+
+pub fn reset_mtime(repo: Repository) -> Result<FileSet> {
+    let candidates = find_candidates(&repo)?;
+    let workdir_files = find_files(&repo)?;
+    let f: HashSet<_> = workdir_files.intersection(&candidates).collect();
+    let touched = touch(&repo, f)?;
+    Ok(touched)
+}
+
+/// Get repository object from current working directory
+pub fn get_repo() -> Result<Repository> {
+    Ok(Repository::discover("./")?)
+}
+
+fn find_candidates(_repo: &Repository) -> Result<FileSet> {
+    let candidates = FileSet::new();
+    Ok(candidates)
+}
+
+fn find_files(_repo: &Repository) -> Result<FileSet> {
+    let workdir_files = FileSet::new();
+    Ok(workdir_files)
+}
+
+fn touch(_repo: &Repository, _touchables: HashSet<&String>) -> Result<FileSet> {
+    let touched = FileSet::new();
+    Ok(touched)
+}
