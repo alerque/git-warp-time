2f57e8fcd211ba8b7804e67dfde2bfe8c304c8cd
Caleb Maclennan <caleb@alerque.com>
2021-04-08 09:59:47 +0300

    fix: Move CLI versioning out of lib into bin only

--- src/bin/git-warp-time.rs
+++ src/bin/git-warp-time.rs
@@ -1,7 +1,10 @@
 use clap::IntoApp;
 use git_warp_time::cli::Cli;
+use git_warp_time::Result;
 use git_warp_time::{get_repo, reset_mtime};
-use git_warp_time::{Result, VERSION};
+
+/// CLI version number as detected by `git describe --tags` at build time
+pub static VERSION: &str = env!("VERGEN_GIT_SEMVER");
 
 fn main() -> Result<()> {
     let app = Cli::into_app().version(VERSION);
--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,6 @@ use std::{error, fs, result};
 
 pub mod cli;
 
-/// CLI version number as detected by `git describe --tags` at build time
-pub static VERSION: &str = env!("VERGEN_GIT_SEMVER");
-
 pub type Result<T> = result::Result<T, Box<dyn error::Error>>;
 
 type FileSet = HashSet<String>;
