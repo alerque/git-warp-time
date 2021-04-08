f02ee8819791a567ec6f0ab51d75be25ba876364
Caleb Maclennan <caleb@alerque.com>
2021-04-08 10:54:15 +0300

    fix: Correct env variable to reflect renamed app

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use vergen::vergen;
 fn main() {
     let mut flags = vergen::Config::default();
     // If passed a version, use that instead of vergen's formatting
-    if let Ok(val) = env::var("GTW_VERSION") {
+    if let Ok(val) = env::var("GWT_VERSION") {
         *flags.git_mut().semver_mut() = false;
         println!("cargo:rustc-env=VERGEN_GIT_SEMVER={}", val)
     };
