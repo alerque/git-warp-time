ff484cef199c49bab6235047a1c18e4b0528a710
Caleb Maclennan <caleb@alerque.com>
2021-04-08 11:17:14 +0300

    feat: Mask binary only dependencies behind feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use std::collections::HashSet;
 use std::path::Path;
 use std::{error, fs, result};
 
+#[cfg(feature = "cli")]
 pub mod cli;
 
 pub type Result<T> = result::Result<T, Box<dyn error::Error>>;
