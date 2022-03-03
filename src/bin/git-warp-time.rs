b1efa384c0eed3f27f3f84f27c578700b757f835
Caleb Maclennan <caleb@alerque.com>
2022-03-03 13:27:29 +0300

    refactor(cli): Replace deprecated calls in Clap usage

--- src/bin/git-warp-time.rs
+++ src/bin/git-warp-time.rs
@@ -6,7 +6,7 @@ use git_warp_time::{get_repo, reset_mtime};
 
 fn main() -> git_warp_time::Result<()> {
     let version = option_env!("VERGEN_GIT_SEMVER").unwrap_or_else(|| env!("VERGEN_BUILD_SEMVER"));
-    let app = Cli::into_app().version(version);
+    let app = Cli::command().version(version);
     let matches = app.get_matches();
     let positionals = matches.values_of("paths");
     let repo = get_repo().unwrap();
