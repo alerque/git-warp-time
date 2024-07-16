57c9996d14bd01dcfcd3750aa171c4e456027df6
Caleb Maclennan <caleb@alerque.com>
2024-07-16 22:40:46 +0300

    feat(cli): Return readable messages on runtime errors

--- src/bin/git-warp-time.rs
+++ src/bin/git-warp-time.rs
@@ -4,15 +4,43 @@ use git_warp_time::cli::Cli;
 use git_warp_time::FileSet;
 use git_warp_time::{get_repo, reset_mtimes, resolve_repo_path};
 
-use std::io::{Error, ErrorKind};
+use snafu::prelude::*;
 use std::path::Path;
 
-fn main() -> git_warp_time::Result<()> {
+#[derive(Snafu)]
+enum Error {
+    #[snafu(display(
+        "Current working directory is not a valid Git repository.\nDetails: {}",
+        source
+    ))]
+    NoRepository { source: Box<dyn snafu::Error> },
+
+    #[snafu(display("Unable to access repository history.\nDetails: {}", source))]
+    CouldNotAccessRepository { source: Box<dyn snafu::Error> },
+
+    #[snafu(display("Unable to change modification time of files.\nDetails: {}", source))]
+    UnableToResetMTime { source: Box<dyn snafu::Error> },
+
+    #[snafu(display("Path '{}' does not exist", path))]
+    PathNotFound { path: String },
+}
+
+// CLI errors are reported using the Debug trait, but Snafu sets up the Display tait. So we
+// deligate. c.f. https://github.com/shepmaster/snafu/issues/110
+impl std::fmt::Debug for Error {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        std::fmt::Display::fmt(self, fmt)
+    }
+}
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+fn main() -> Result<()> {
     let version = option_env!("VERGEN_GIT_DESCRIBE").unwrap_or_else(|| env!("CARGO_PKG_VERSION"));
     let app = Cli::command().version(version);
     let matches = app.get_matches();
     let positionals = matches.get_many::<String>("paths");
-    let repo = get_repo().unwrap();
+    let repo = get_repo().context(NoRepositorySnafu)?;
     let mut opts = git_warp_time::Options::new()
         .dirty(matches.get_flag("dirty"))
         .ignored(matches.get_flag("ignored"))
@@ -21,14 +49,14 @@ fn main() -> git_warp_time::Result<()> {
         let mut paths: FileSet = FileSet::new();
         for path in positionals.unwrap() {
             if !Path::new(path).exists() {
-                let path_error = format!("Path {path} does not exist");
-                return Err(Box::new(Error::new(ErrorKind::NotFound, path_error)));
+                return PathNotFoundSnafu { path: path.clone() }.fail();
             }
-            let path = resolve_repo_path(&repo, path)?;
+            let path = resolve_repo_path(&repo, path).context(CouldNotAccessRepositorySnafu)?;
+            dbg!(path.clone());
             paths.insert(path);
         }
         opts = opts.paths(Some(paths));
     }
-    reset_mtimes(repo, opts)?;
+    reset_mtimes(repo, opts).context(UnableToResetMTimeSnafu)?;
     Ok(())
 }
