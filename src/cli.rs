4d20ad14e881791293996d006ec586861704cf39
Caleb Maclennan <caleb@alerque.com>
2024-08-12 13:04:09 +0300

    refactor(cli): Update clap usage

--- src/cli.rs
+++ src/cli.rs
@@ -6,22 +6,22 @@ use clap::Parser;
 /// CLI utility that resets the timestamps of files in a Git repository working directory
 /// to the exact timestamp of the last commit which modified each file.
 #[derive(Parser, Debug)]
-#[clap(author, bin_name = "git-warp-time")]
+#[command(author, bin_name = "git-warp-time")]
 pub struct Cli {
     /// Include files tracked by Git but modifications in the working tee
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub dirty: bool,
 
     /// Include files tracked by Git but also ignored
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub ignored: bool,
 
     /// Only touch files that are newer than their history, ignore ones that are older
-    #[clap(short = 'o', long)]
+    #[arg(short = 'o', long)]
     pub ignore_older: bool,
 
     /// Don't print any output about files touched or skipped
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub quiet: bool,
 
     /// Optional list of paths to operate on instead of default which is all files tracked by Git
